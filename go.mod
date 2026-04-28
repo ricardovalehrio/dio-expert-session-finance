d0e5f4b871bd941d57fd5a67057fbd7760916a50
ricardovaleri0 <ricardovaleriosilvaoliveira@gmail.com>
2026-04-27 21:16:41 -0300

    Correção de pacotes e inicialização do servidor

--- go.mod
+++ go.mod
@@ -2,17 +2,17 @@ module github.com/ricardovalehrio/dio-expert-session-finance
 
 go 1.26.2
 
+require github.com/prometheus/client_golang v1.23.2
+
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/prometheus/client_golang v1.23.2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.66.1 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
-	golang.org/x/lint v0.0.0-20241112194109-818c5a804067 // indirect
 	golang.org/x/sys v0.43.0 // indirect
-	golang.org/x/tools v0.44.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,73 @@
+// Arquivo: main.go (na raiz do projeto)
 package main
 
 import (
-	"github.com/ricardovalehrio/dio-expert-session-finance/adapter/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+	// O caminho deve bater exatamente com o module no seu go.mod
+	"github.com/ricardovalehrio/dio-expert-session-finance/model/transaction"
 )
 
 func main() {
-	http.Init()
+	// Definindo as rotas
+	http.HandleFunc("/transactions", getTransactions)
+	http.HandleFunc("/transactions/create", createATransaction)
+
+	fmt.Println("Servidor iniciado com sucesso na porta 8080...")
+	// Inicia o servidor
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Erro ao iniciar o servidor: %s\n", err)
+	}
+}
+
+func getTransactions(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	layout := "2006-01-02T15:04:05"
+	salaryReceived, _ := time.Parse(layout, "2026-04-26T14:56:05")
+
+	// Criando uma lista de exemplo usando o pacote transaction
+	minhasTransacoes := transaction.Transactions{
+		transaction.Transaction{
+			Title:     "Salário Mensal",
+			Amount:    1200.0,
+			Type:      0,
+			CreatedAt: salaryReceived,
+		},
+	}
+
+	json.NewEncoder(w).Encode(minhasTransacoes)
+}
+
+func createATransaction(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var novaTransacao transaction.Transaction
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Converte o JSON recebido para a struct
+	err = json.Unmarshal(body, &novaTransacao)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Printf("Transação recebida: %+v\n", novaTransacao)
+	w.WriteHeader(http.StatusCreated)
 }
